cacb7815b4309960bd1dd5fb06ffd3b5d4f4b1be
Boris Alexeev <boris.alexeev@gmail.com>
2026-03-18 20:25:52 -0400

    MO508718c statement

--- src/v4.24.0/MathOverflow/MO508718c_statement.lean
+++ src/v4.24.0/MathOverflow/MO508718c_statement.lean
@@ -2,8 +2,6 @@ import Mathlib
 
 noncomputable def trigamma (z : ℝ) : ℝ := ∑' (n : ℕ), 1 / (n + z)^2
 
-noncomputable def F (z : ℝ) : ℝ := ∑' k : ℕ, if k = 0 then 0 else z^k / ((k : ℝ)^2 * (Nat.choose (2 * k) k : ℝ))
-
 noncomputable def S_sum : ℝ := ∑' k : ℕ, if k = 0 then 0 else (harmonic (2 * k) - harmonic k : ℝ) / ((k : ℝ) ^ 2 * (Nat.choose (2 * k) k : ℝ))
 
 theorem main_theorem : S_sum = 2 * riemannZeta 3 - (Real.pi * Real.sqrt 3 / 24) * trigamma (1/3) + (Real.pi * Real.sqrt 3 / 72) * trigamma (5/6) := by
