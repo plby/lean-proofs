5a807dd64ec387dd73e08717cf85c1523823bbec
Boris Alexeev <boris.alexeev@gmail.com>
2026-04-27 22:57:05 -0700

    Add Erdos105.

--- src/latest/ErdosProblems/All.lean
+++ src/latest/ErdosProblems/All.lean
@@ -6,6 +6,7 @@ import ErdosProblems.Erdos34
 import ErdosProblems.Erdos56
 import ErdosProblems.Erdos93
 import ErdosProblems.Erdos94
+import ErdosProblems.Erdos105
 import ErdosProblems.Erdos164
 import ErdosProblems.Erdos990
 import ErdosProblems.Erdos990b
