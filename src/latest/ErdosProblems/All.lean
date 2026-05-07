e18074cf51c43632d98ec6fb1eb8db7a39f8e66b
Boris Alexeev <boris.alexeev@gmail.com>
2026-05-07 06:50:56 -0700

    Add Erdos{1000,1008,1022}.

--- src/latest/ErdosProblems/All.lean
+++ src/latest/ErdosProblems/All.lean
@@ -106,7 +106,10 @@ import ErdosProblems.Erdos974
 import ErdosProblems.Erdos990
 import ErdosProblems.Erdos990b
 import ErdosProblems.Erdos997
+import ErdosProblems.Erdos1000
+import ErdosProblems.Erdos1008
 import ErdosProblems.Erdos1014
+import ErdosProblems.Erdos1022
 import ErdosProblems.Erdos1044
 import ErdosProblems.Erdos1090
 import ErdosProblems.Erdos1098
