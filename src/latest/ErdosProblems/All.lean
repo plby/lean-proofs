24302c90c064c5bdffd845ad45574f06607e420f
Boris Alexeev <boris.alexeev@gmail.com>
2026-05-12 23:08:27 -0700

    Add Erdos729.

--- src/latest/ErdosProblems/All.lean
+++ src/latest/ErdosProblems/All.lean
@@ -128,6 +128,7 @@ import ErdosProblems.Erdos707
 import ErdosProblems.Erdos728
 import ErdosProblems.Erdos728b
 import ErdosProblems.Erdos728p
+import ErdosProblems.Erdos729
 import ErdosProblems.Erdos753
 import ErdosProblems.Erdos756
 import ErdosProblems.Erdos760
