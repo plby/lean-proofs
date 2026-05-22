77db825a7a8632a1da91f03681a98358a0a7233b
Boris Alexeev <boris.alexeev@gmail.com>
2026-05-22 09:11:18 -0700

    Clean up Erdos502.

--- src/latest/ErdosProblems/Erdos502.lean
+++ src/latest/ErdosProblems/Erdos502.lean
@@ -38,18 +38,20 @@ import Mathlib
 
 namespace Erdos502
 
-set_option linter.mathlibStandardSet false
+set_option linter.style.setOption false
+set_option linter.style.openClassical false
+set_option linter.style.longLine false
+set_option linter.flexible false
+set_option linter.style.refine false
+set_option linter.style.cases false
+set_option linter.style.multiGoal false
+set_option linter.unusedDecidableInType false
 
 open scoped BigOperators
 open scoped Real
 open scoped Nat
 open scoped Classical
 open scoped Pointwise
-
-set_option maxHeartbeats 0
-set_option maxRecDepth 4000
-set_option synthInstance.maxHeartbeats 20000
-set_option synthInstance.maxSize 128
 
 set_option relaxedAutoImplicit false
 set_option autoImplicit false
