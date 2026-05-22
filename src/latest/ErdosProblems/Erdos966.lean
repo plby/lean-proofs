f3952befa397ad1f26ef7e9f46d82e4e0c1c2a5f
Boris Alexeev <boris.alexeev@gmail.com>
2026-05-22 11:10:45 -0700

    Clean up Erdos966.

--- src/latest/ErdosProblems/Erdos966.lean
+++ src/latest/ErdosProblems/Erdos966.lean
@@ -35,21 +35,22 @@ import Mathlib
 
 namespace Erdos966
 
-
-set_option linter.mathlibStandardSet false
+set_option linter.style.setOption false
+set_option linter.style.openClassical false
+set_option linter.style.longLine false
+set_option linter.style.induction false
+set_option linter.style.multiGoal false
+set_option linter.style.refine false
+set_option linter.flexible false
 set_option linter.unreachableTactic false
 set_option linter.unusedTactic false
+
 
 open scoped BigOperators
 open scoped Real
 open scoped Nat
 open scoped Classical
 open scoped Pointwise
-
-set_option maxHeartbeats 0
-set_option maxRecDepth 4000
-set_option synthInstance.maxHeartbeats 20000
-set_option synthInstance.maxSize 128
 
 set_option relaxedAutoImplicit false
 set_option autoImplicit false
