9101f1144e6214bbb406a3749c839edbacc03e1e
Boris Alexeev <boris.alexeev@gmail.com>
2026-05-30 00:27:31 -0700

    Address linter.style.openClassical in Erdos678.

--- src/latest/ErdosProblems/Erdos678.lean
+++ src/latest/ErdosProblems/Erdos678.lean
@@ -27,7 +27,6 @@ import PrimeNumberTheoremAnd.Consequences
 namespace Erdos678
 
 set_option linter.style.setOption false
-set_option linter.style.openClassical false
 set_option linter.style.longLine false
 set_option linter.flexible false
 set_option linter.style.induction false
@@ -39,8 +38,9 @@ set_option maxHeartbeats 1000000
 
 open scoped BigOperators
 open scoped Nat
-open scoped Classical
 open scoped Pointwise
+
+attribute [local instance] Classical.propDecidable
 
 set_option linter.unusedSimpArgs false
 set_option linter.unusedVariables false
